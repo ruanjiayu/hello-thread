0e272620359e9278599ec7329dd31c92ef0bb77c
ruanjiayu <1963241849@qq.com>
2020-02-16 13:31:47 +0800

    在没有锁和队列的情况下，去银行取款会出现线程不安全事件。使用了sleep来模拟业务的相关操作和网络延迟，放大问题的发生性

---
+++ src/main/java/com/fun/uncle/hello/thread/syn/UnsafeBank.java
@@ -0,0 +1,90 @@
+package com.fun.uncle.hello.thread.syn;
+
+/**
+ * @Description: 不安全的取钱。
+ * 两个人同时去银行取钱，账户
+ * @Author: Xian
+ * @CreateDate: 2020/2/16  13:11
+ * @Version: 0.0.1-SNAPSHOT
+ */
+public class UnsafeBank {
+
+    public static void main(String[] args) {
+        Account account = new Account("老婆本", 100);
+        Drawing you = new Drawing(account, 50);
+        Drawing girlFriend = new Drawing(account, 80);
+
+        new Thread(you, "xian").start();
+        new Thread(girlFriend, "fan").start();
+
+    }
+
+}
+
+/**
+ * 账户
+ */
+class Account{
+    private String name;
+    private int money;
+
+    public Account(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
+
+class Drawing implements Runnable{
+    /**
+     * 账户
+     */
+    private Account account;
+
+    /**
+     * 取了多少钱
+     */
+    private int drawingMoney;
+
+    /**
+     * 现在手上有多少钱
+     */
+    private int nonMoney;
+
+    public Drawing(Account account, int drawingMoney) {
+        this.account = account;
+        this.drawingMoney = drawingMoney;
+    }
+
+    @Override
+    public void run() {
+        // 判断卡里面余额是否足够
+        if (account.getMoney() - drawingMoney <= 0) {
+            System.out.println(Thread.currentThread().getName() + "账户余额不足，不能取钱");
+            return;
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 卡内余额 = 余额 - 你去的钱
+        account.setMoney(account.getMoney() - drawingMoney);
+        // 手上的钱
+        nonMoney = nonMoney + drawingMoney;
+        System.out.println(account.getName() + "账户余额为" + account.getMoney());
+        System.out.println(Thread.currentThread().getName() + "拿到手上有" + nonMoney);
+    }
+}
